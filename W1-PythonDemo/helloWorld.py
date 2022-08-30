38ec3887e5a6ef9b03edf44219377fd3c1744e71
Paul Talaga <talagap@uindy.edu>
2022-08-30 15:52:04 -0400

    Python Demo Aug 30

---
+++ W1-PythonDemo/helloWorld.py
@@ -0,0 +1,17 @@
+
+def addStuff(a,b):
+	return a + b
+
+print("Hello World %d and then %d, %s" % (5,567,"bob")   )
+
+a = 5
+b = 10
+print(a + b)
+
+b = 7.8
+print(a + b)
+thing = [5,9,6]
+for i in range(10,0,-1):    # for(int i = 0; i < 10; i++)
+	print(i)
+
+print(addStuff(5,6))
---
+++ W1-PythonDemo/passing.py
@@ -0,0 +1,13 @@
+
+def printIt(it):
+	#it.append(55)
+	#it = 7
+	it[-1] = "Nothing to see here"
+	print(it)
+
+list = [5,"bob"] #[5,6,7,8]
+
+
+print(list)
+printIt(list)
+print(list)
