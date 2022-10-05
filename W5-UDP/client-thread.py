47baf437674e15dcd94265d6a655cb7e5f036754
Paul Talaga <talagap@uindy.edu>
2022-10-05 11:03:27 -0400

    Windows fix

--- W5-UDP/client-thread.py
+++ W5-UDP/client-thread.py
@@ -6,8 +6,13 @@ ip_address = '127.0.0.1'
 #ip_address = '10.17.102.208'
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock_ready = False
 
 def listenThread():
+	global s
+	global sock_ready
+	while not sock_ready:
+		pass
 	while True:
 		(msg, addr) = s.recvfrom(1024)
 		msg = msg.decode('ascii')
@@ -19,5 +24,6 @@ listener.start()
 while True:
 	message = input("Give me some text")
 	s.sendto(message.encode('ascii'), (ip_address, port_number))
+	sock_ready = True
 
 	
